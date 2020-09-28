a03af489b36fb936a6e57974716efb2859f14880
josomm22 <josommefeld@icloud.com>
2020-09-28 19:44:15 +0300

    added https

--- src/API/fetch.js
+++ src/API/fetch.js
@@ -2,14 +2,15 @@ import axios from 'axios';
 import {message} from 'antd';
 
 const API_KEY = "VlbDqUWC7TBfuUqAAKJTcQc9EFAJytm5";
-const corsUrl = "https://cors-anywhere.herokuapp.com/";
+// const corsUrl = "https://cors-anywhere.herokuapp.com/";
+const corsUrl = "";
 
 // const testValueCurrentWeather = [
 //     {"LocalObservationDateTime":"2020-09-25T16:16:00+03:00","EpochTime":1601039760,"WeatherText":"Clouds and sun","WeatherIcon":4,"HasPrecipitation":false,"PrecipitationType":null,"IsDayTime":true,"Temperature":{"Metric":{"Value":29.4,"Unit":"C","UnitType":17},"Imperial":{"Value":85.0,"Unit":"F","UnitType":18}},"MobileLink":"http://m.accuweather.com/en/il/tel-aviv/215854/current-weather/215854?lang=en-us","Link":"http://www.accuweather.com/en/il/tel-aviv/215854/current-weather/215854?lang=en-us"}
 // ]
 
 export async function handleSearch(value) {
-    const url = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete"
+    const url = "https://dataservice.accuweather.com/locations/v1/cities/autocomplete"
     try {
         const response = await axios.get(`${corsUrl}${url}?apikey=${API_KEY}&q=${value}`);
         return response.data;
@@ -22,7 +23,7 @@ export async function handleSearch(value) {
 };
 
 export async function getCurrentConditions(id) {
-    const url = "http://dataservice.accuweather.com/currentconditions/v1/"
+    const url = "https://dataservice.accuweather.com/currentconditions/v1/"
     try {
         const response = await axios.get(`${corsUrl}${url}${id}?apikey=${API_KEY}`);
         return response.data;
@@ -36,7 +37,7 @@ export async function getCurrentConditions(id) {
 };
 
 export async function get5DayForecast(id) {
-    const url = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"
+    const url = "https://dataservice.accuweather.com/forecasts/v1/daily/5day/"
     try {
         const response = await axios.get(`${corsUrl}${url}${id}?apikey=${API_KEY}&metric=true`);
         return response.data;
@@ -48,7 +49,7 @@ export async function get5DayForecast(id) {
 }
 
 export async function fetchLocation(obj) {
-    const url = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search"
+    const url = "https://dataservice.accuweather.com/locations/v1/cities/geoposition/search"
     try {
         const response = await axios.get(`${corsUrl}${url}?apikey=${API_KEY}&q=${obj.latitude+','+obj.longitude}`);
         return response.data;
